684af7e482c5c7a907426b266441790e1735c4af
Jon Bardin <diclophis@gmail.com>
2010-02-15 09:29:23 +0000

    fixes for camping, and a working RisingCode

--- config.ru
+++ config.ru
@@ -1,2 +1,17 @@
 #!/usr/bin/env ruby
 
+HOME = "/home/ubuntu"
+
+require "cloud_collect"
+require "camping_response_headers_fix"
+require HOME + "/risingcode/risingcode"
+
+map "http://cloudcollect.com/" do
+  use CampingResponseHeadersFix
+  run Rack::Adapter::Camping.new(CloudCollect)
+end
+
+map "http://risingcode.com/" do
+  use CampingResponseHeadersFix
+  run Rack::Adapter::Camping.new(RisingCode)
+end
---
+++ install_gems.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+gem install tzinfo builder memcache-client rack rack-test rack-mount erubis mail text-format thor bundler i18n
+sudo gem install camping
+gem install rack-mount -v 0.4.0
+sudo gem install rake -v 0.8.3
+gem install rails --pre
+gem install rmagick
+gem install RedCloth
+gem install daemons
+gem install ruby2ruby
+gem install uuidtools
+gem install right_aws
+gem install linguistics
+gem install hpricot
+gem install plist
+gem install ruby-openid
+gem install json
