873c6a81742c1e71fb9c35a6c6401b7efbc0e340
Diclophis <diclophis@gmail.com>
2012-04-13 07:38:33 +0000

    cleanup some stuff

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sudo apt-get install build-essential ruby ri rdoc irb ruby-dev mysql-client mysql-server memcached libmysqlclient-dev git-core imagemagick libmagick9-dev libopenssl-ruby1.8
-
-sudo ln -s /usr/bin/gem1.8 /usr/local/bin/gem
-sudo ln -s /usr/bin/ruby1.8 /usr/local/bin/ruby
-sudo ln -s /usr/bin/rdoc1.8 /usr/local/bin/rdoc
-sudo ln -s /usr/bin/ri1.8 /usr/local/bin/ri
-sudo ln -s /usr/bin/irb1.8 /usr/local/bin/irb
